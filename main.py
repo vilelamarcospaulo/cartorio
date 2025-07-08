c4b8605bb664c6c0d9575d081bbc481357c6ccbf
marcos vilela <marcospaulo.vilelasantos@gmail.com>
2025-07-08 09:49:54 -0300

    expose path generic by envvar

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from lib.processor import process_folder
 
 import logging
+import argparse
+import os.path
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[logging.StreamHandler()])
 
-process_folder('/Users/vilelamarcos/tmp/')
+def main():
+    parser = argparse.ArgumentParser(description='Process a folder')
+    parser.add_argument('path', help='Path to the folder to process')
+    args = parser.parse_args()
+    
+    # Normalize path for cross-platform compatibility
+    normalized_path = os.path.normpath(args.path)
+    process_folder(normalized_path)
+
+if __name__ == '__main__':
+    main()
